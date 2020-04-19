07be62fa2edbb426abacfc5f781468046646a370
Mike Seddon <seddonm1@gmail.com>
2020-04-19 15:27:32 +1000

    1.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,11 +8,11 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.7" % "test,it"
 
   // arc
-  val arc = "ai.tripl" %% "arc" % "2.8.0" % "provided"
+  val arc = "ai.tripl" %% "arc" % "2.10.0" % "provided"
 
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
 
-  val libPhoneNumber = "com.googlecode.libphonenumber" % "libphonenumber" % "8.12.0"
+  val libPhoneNumber = "com.googlecode.libphonenumber" % "libphonenumber" % "8.12.1"
 
   // Project
   val etlDeps = Seq(
